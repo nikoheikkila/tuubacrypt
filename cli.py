d0a9439d61df8c1512d6c94e3482129e6ed29747
Niko Heikkilä <yo@nikoheikkila.fi>
2020-03-14 15:13:29 +0200

    feat: add CLI tool

---
+++ cli.py
@@ -0,0 +1,41 @@
+import io
+import click
+
+from typing import Optional
+from tuubacrypt import TuubaCrypt
+
+def println(text: str) -> None:
+    click.echo(text, nl=False)
+
+def get_stream() -> str:
+    return "".join(click.get_text_stream('stdin').readlines())
+
+@click.group()
+@click.version_option()
+@click.pass_context
+def main(ctx: click.Context) -> None:
+    """The main command for CLI usage."""
+    ctx.ensure_object(dict)
+    ctx.obj['tcrypt'] = TuubaCrypt()
+
+@main.command()
+@click.pass_context
+@click.argument('text', type=click.STRING, required=False)
+def encrypt(ctx: click.Context, text):
+    tcrypt = ctx.obj['tcrypt']
+
+    if not text:
+        text = get_stream()
+
+    println(tcrypt.encrypt(text))
+
+@main.command()
+@click.pass_context
+@click.argument('text', type=click.STRING, required=False)
+def decrypt(ctx: click.Context, text: str):
+    tcrypt = ctx.obj['tcrypt']
+
+    if not text:
+        text = get_stream()
+
+    println(tcrypt.decrypt(text))
